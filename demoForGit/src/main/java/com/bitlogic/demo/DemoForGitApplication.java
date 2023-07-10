a9a0bea200ab557eca3fdb86e2d823eb14c76c2a
akshay25031998 <akshayrajput7057@gmail.com>
2023-07-10 03:25:10 -0700

    changes for akshay in main method

--- demoForGit/src/main/java/com/bitlogic/demo/DemoForGitApplication.java
+++ demoForGit/src/main/java/com/bitlogic/demo/DemoForGitApplication.java
@@ -9,6 +9,7 @@ public class DemoForGitApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoForGitApplication.class, args);
 		System.out.println("This is Ecclipse IDE");
+		System.out.println("Akshay eclips");
 	}
 
 }
